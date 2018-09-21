2493a3ab5b0f3892c04bf9580f5cea612ef54eae
Lea Ann Bradford <leaann@notch8.com>
2018-09-21 14:08:10 -0700

    moved html_safe out of the css file

--- app/forms/hyrax/forms/admin/appearance.rb
+++ app/forms/hyrax/forms/admin/appearance.rb
@@ -96,7 +96,7 @@ module Hyrax
 
         # The custom css module
         def custom_css_block
-          block_for('custom_css_block', '/* custom stylesheet */')
+          block_for('custom_css_block', '/* custom stylesheet */').html_safe
         end
 
         # Persist the form values

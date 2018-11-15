543530501e569a8d370c3a96413aecd33c41b2b7
Rob Kaufman <rob@notch8.com>
2018-11-14 23:13:53 -0800

    make session stick across domains, make invitiations more intutive in new model

--- app/controllers/hyku/invitations_controller.rb
+++ app/controllers/hyku/invitations_controller.rb
@@ -5,5 +5,23 @@ module Hyku
     def after_invite_path_for(_resource)
       hyrax.admin_users_path
     end
+
+    # override the standard invite so that accounts are added properly
+    # if they already exist on another tenant and invited if they do not
+    def create
+      respond_to do |format|
+        if current_account.update(account_params)
+          format.html { redirect_to hyrax.admin_users_path, notice: 'Account was successfully updated.' }
+        else
+          format.html { redirect_to hyrax.admin_users_path, notice: 'Account could not be added.' }
+        end
+      end
+    end
+
+    protected
+    def account_params
+      params.require(:account).permit(admin_emails: [])
+    end
+
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_hyku_session'
+Rails.application.config.session_store :cookie_store, key: '_hyku_session', domain: ".#{ENV['SETTINGS__MULTITENANCY__ADMIN_HOST']}"

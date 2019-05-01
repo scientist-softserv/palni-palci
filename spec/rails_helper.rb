samvera/hyku

Overhauled Docker configuration and modified specs to allow to run inside Docker
This commit is in preperation of beginning to release docker images that can be used via Docker Hub with each new release. It also allows for fully dockerized development environment and production environments.

Two phase Docker build - base and main. The goal here are much faster build times by moving seldom changing items to the base image.

Moves to Passenger base image which allows a few real niceties:

  - for dev, you can restart the web process w/o restarting the container, saving a lot of time
  - for prod passenger is more effecient by default in a variety of configurations than puma. Puma frequenty requires more tuning
  - means nginx is serving assets, so no extra proxy is required

Note, this makes the build a little bigger, but with libreoffice and fits in there this change is a pretty small fraction in my opinion.

Uses a nulldb to compile assets during build, making deployment much faster. Also set up to allow for optional assets volume, which makes keeping cached assets possible

This Dockerfile moves fits to always be in the same place in the container. There is still a version file present, so we don't really need it in the path and that means we don't have to update multiple places when we change versions. Just
 in the Dockerfile.base

Sets the admin and default host in dockerfile and virtual_host attributes. That allows this setup to work in dev by default with common proxies like Dory and others.

Makes test adjustments to allow for Docker environment, adds an external Chrome driver docker container to the dev environment

Splits the production and dev docker-compose.yml files. Reduced dev zookeeper to 1, removed lb and memcached from dev. Splitting futher refinement of production docker-compose in to a seperate task.

spec/rails_helper.rb
@@ -1,5 +1,12 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+# This file is cepied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+
+# In test most, unset some variables that can cause trouble
+# before booting up Rails
+ENV['SETTINGS__MULTITENANCY__ADMIN_HOST'] = nil
+ENV['SETTINGS__MULTITENANCY__ADMIN_ONLY_TENANT_CREATION'] = nil
+ENV['SETTINGS__MULTITENANCY__DEFAULT_HOST'] = nil
+ENV['SETTINGS__MULTITENANCY__ENABLED'] = nil
 
 require 'simplecov'
 require 'coveralls'
@@ -55,19 +62,50 @@ ActiveRecord::Migration.maintain_test_schema!
 # Uses faster rack_test driver when JavaScript support not needed
 Capybara.default_driver = :rack_test
 
-Capybara.register_driver :headless_chrome do |app|
+if ENV['CHROME_HOSTNAME'].present?
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: {
+      args: %w[headless disable-gpu no-sandbox whitelisted-ips window-size=1400,1400]
+    }
+  )
+
+  Capybara.register_driver :chrome do |app|
+    d = Capybara::Selenium::Driver.new(app,
+                                       browser: :remote,
+                                       desired_capabilities: capabilities,
+                                       url: "http://#{ENV['CHROME_HOSTNAME']}:4444/wd/hub")
+    # Fix for capybara vs remote files. Selenium handles this for us
+    d.browser.file_detector = lambda do |args|
+      str = args.first.to_s
+      str if File.exist?(str)
+    end
+    d
+  end
+  Capybara.server_host = '0.0.0.0'
+  Capybara.server_port = 3001
+  ENV['WEB_HOST'] ||= if ENV['IN_DOCKER']
+                        'web'
+                      else
+                        `hostname -s`.strip
+                      end
+  Capybara.app_host = "http://#{ENV['WEB_HOST']}:#{Capybara.server_port}"
+else
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
     chromeOptions: {
       args: %w[headless disable-gpu]
     }
   )
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    desired_capabilities: capabilities
-  )
+
+  Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      desired_capabilities: capabilities
+    )
+  end
 end
-Capybara.javascript_driver = :headless_chrome
+
+Capybara.javascript_driver = :chrome
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -106,6 +144,7 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
+    CreateSolrCollectionJob.new.without_account('hyku-test') if ENV['IN_DOCKER']
   end
 
   config.before(:each) do |example|

scientist-softserv/palni-palci

spec/rails_helper.rb
@@ -1,5 +1,12 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+# This file is cepied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+
+# In test most, unset some variables that can cause trouble
+# before booting up Rails
+ENV['SETTINGS__MULTITENANCY__ADMIN_HOST'] = nil
+ENV['SETTINGS__MULTITENANCY__ADMIN_ONLY_TENANT_CREATION'] = nil
+ENV['SETTINGS__MULTITENANCY__DEFAULT_HOST'] = nil
+ENV['SETTINGS__MULTITENANCY__ENABLED'] = nil
 
 require 'simplecov'
 require 'coveralls'
@@ -55,19 +62,50 @@ ActiveRecord::Migration.maintain_test_schema!
 # Uses faster rack_test driver when JavaScript support not needed
 Capybara.default_driver = :rack_test
 
-Capybara.register_driver :headless_chrome do |app|
+if ENV['CHROME_HOSTNAME'].present?
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: {
+      args: %w[headless disable-gpu no-sandbox whitelisted-ips window-size=1400,1400]
+    }
+  )
+
+  Capybara.register_driver :chrome do |app|
+    d = Capybara::Selenium::Driver.new(app,
+                                       browser: :remote,
+                                       desired_capabilities: capabilities,
+                                       url: "http://#{ENV['CHROME_HOSTNAME']}:4444/wd/hub")
+    # Fix for capybara vs remote files. Selenium handles this for us
+    d.browser.file_detector = lambda do |args|
+      str = args.first.to_s
+      str if File.exist?(str)
+    end
+    d
+  end
+  Capybara.server_host = '0.0.0.0'
+  Capybara.server_port = 3001
+  ENV['WEB_HOST'] ||= if ENV['IN_DOCKER']
+                        'web'
+                      else
+                        `hostname -s`.strip
+                      end
+  Capybara.app_host = "http://#{ENV['WEB_HOST']}:#{Capybara.server_port}"
+else
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
     chromeOptions: {
       args: %w[headless disable-gpu]
     }
   )
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    desired_capabilities: capabilities
-  )
+
+  Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      desired_capabilities: capabilities
+    )
+  end
 end
-Capybara.javascript_driver = :headless_chrome
+
+Capybara.javascript_driver = :chrome
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -106,6 +144,7 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
+    CreateSolrCollectionJob.new.without_account('hydra-test') if ENV['IN_DOCKER']
   end
 
   config.before(:each) do |example|
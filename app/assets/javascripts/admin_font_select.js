6bd08f2dbfd4cadd68ecc94d8371f67487e1a3bb
Rob Kaufman <rob@notch8.com>
2019-05-21 18:01:18 -0700

    css and js changes for font select

--- app/assets/javascripts/admin_font_select.js
+++ app/assets/javascripts/admin_font_select.js
@@ -1,5 +1,6 @@
 $(document).on('turbolinks:load', function() {
   $("#admin_appearance_body_font").fontselect({lookahead: 20})
   $("#admin_appearance_headline_font").fontselect({lookahead: 20})
+  console.log('here')
 });
 

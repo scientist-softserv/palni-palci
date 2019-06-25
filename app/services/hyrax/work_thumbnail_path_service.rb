4bf64d6f76ceabdb21ae60fe82b36a359f9e6e5c
Rob Kaufman <rob@notch8.com>
2019-06-25 09:19:10 -0700

    one more fix

--- app/services/hyrax/work_thumbnail_path_service.rb
+++ app/services/hyrax/work_thumbnail_path_service.rb
@@ -2,7 +2,7 @@ module Hyrax
   class WorkThumbnailPathService < Hyrax::ThumbnailPathService
     class << self
       def default_image
-        Site.instance.default_work_image
+        Site.instance.default_work_image.present? ? Site.instance.default_work_image : ActionController::Base.helpers.image_path('work.png')
       end
     end
   end

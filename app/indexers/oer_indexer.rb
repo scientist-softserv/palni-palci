61fe82c5f72cdebb53fed7d34903baab01aeb753
Rob Kaufman <rob@notch8.com>
2019-10-25 16:09:38 -0500

    remove unused iiif code

--- app/indexers/oer_indexer.rb
+++ app/indexers/oer_indexer.rb
@@ -9,10 +9,7 @@ class OerIndexer < Hyrax::WorkIndexer
   # this behavior
   include Hyrax::IndexesLinkedMetadata
 
-   # Use thumbnails served by RIIIF
-   self.thumbnail_path_service = IIIFWorkThumbnailPathService
-   
-   # Uncomment this block if you want to add custom indexing behavior:
+  # Uncomment this block if you want to add custom indexing behavior:
   # def generate_solr_document
   #  super.tap do |solr_doc|
   #    solr_doc['my_custom_field_ssim'] = object.my_custom_property

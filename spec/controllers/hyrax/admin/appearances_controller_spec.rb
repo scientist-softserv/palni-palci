bb8e0257bdd25bec3a74cacc4f8ba662a8c2acaa
Benjamin Kiah Stroud <kiah@notch8.com>
2020-05-11 17:02:03 +0000

    i158 specs

--- spec/controllers/hyrax/admin/appearances_controller_spec.rb
+++ spec/controllers/hyrax/admin/appearances_controller_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe Hyrax::Admin::AppearancesController, type: :controller, singleten
 
     describe "GET #show" do
       it "denies the request" do
-        skip
         get :show
         expect(response).to have_http_status(:unauthorized)
       end
@@ -16,7 +15,6 @@ RSpec.describe Hyrax::Admin::AppearancesController, type: :controller, singleten
 
     describe "PUT #update" do
       it "denies the request" do
-        skip
         put :update
         expect(response).to have_http_status(:unauthorized)
       end
@@ -28,7 +26,6 @@ RSpec.describe Hyrax::Admin::AppearancesController, type: :controller, singleten
 
     describe "GET #show" do
       it "assigns the requested site as @site" do
-        skip
         get :show, params: {}
         expect(response).to be_successful
       end
@@ -39,11 +36,28 @@ RSpec.describe Hyrax::Admin::AppearancesController, type: :controller, singleten
 
       context "with valid params" do
         let(:valid_attributes) do
-          { banner_image: "image.jpg", directory_image: "image.jpg" }
+          {
+            logo_image: 'image.jpg',
+            banner_image: 'image.jpg',
+            directory_image: 'image.jpg',
+            default_collection_image: 'image.jpg',
+            default_work_image: 'image.jpg',
+            link_color: '#2e74b2',
+            body_font: 'Helvetica Neue, Helvetica, Arial, sans-serif',
+            custom_css_block: '/* custom stylesheet */'
+          }
+        end
+
+        it 'sets a logo image' do
+          expect(Site.instance.logo_image?).to be false
+          f = fixture_file_upload('/images/nypl-hydra-of-lerna.jpg', 'image/jpg')
+          post :update, params: { admin_appearance: { logo_image: f } }
+          expect(response).to redirect_to(hyrax.admin_appearance_path(locale: 'en'))
+          expect(flash[:notice]).to include('The appearance was successfully updated')
+          expect(Site.instance.logo_image?).to be true
         end
 
         it "sets a banner image" do
-          skip
           expect(Site.instance.banner_image?).to be false
           f = fixture_file_upload('/images/nypl-hydra-of-lerna.jpg', 'image/jpg')
           post :update, params: { admin_appearance: { banner_image: f } }
@@ -53,7 +67,6 @@ RSpec.describe Hyrax::Admin::AppearancesController, type: :controller, singleten
         end
 
         it "sets a directory image" do
-          skip
           expect(Site.instance.directory_image?).to be false
           f = fixture_file_upload('/images/nypl-hydra-of-lerna.jpg', 'image/jpg')
           post :update, params: { admin_appearance: { directory_image: f } }
@@ -62,8 +75,44 @@ RSpec.describe Hyrax::Admin::AppearancesController, type: :controller, singleten
           expect(Site.instance.directory_image?).to be true
         end
 
+        context 'for default collection image' do
+          it 'sets an image' do
+            expect(Site.instance.default_collection_image?).to be false
+            f = fixture_file_upload('/images/nypl-hydra-of-lerna.jpg', 'image/jpg')
+            post :update, params: { admin_appearance: { default_collection_image: f } }
+            expect(response).to redirect_to(hyrax.admin_appearance_path(locale: 'en'))
+            expect(flash[:notice]).to include('The appearance was successfully updated')
+            expect(Site.instance.default_collection_image?).to be true
+          end
+
+          it 'enqueues two jobs' do
+            expect(ReindexCollectionsJob).to receive(:perform_later).once
+            expect(ReindexAdminSetsJob).to receive(:perform_later).once
+
+            f = fixture_file_upload('/images/nypl-hydra-of-lerna.jpg', 'image/jpg')
+            post :update, params: { admin_appearance: { default_collection_image: f } }
+          end
+        end
+
+        context 'for default work image' do
+          it 'sets an image' do
+            expect(Site.instance.default_work_image?).to be false
+            f = fixture_file_upload('/images/nypl-hydra-of-lerna.jpg', 'image/jpg')
+            post :update, params: { admin_appearance: { default_work_image: f } }
+            expect(response).to redirect_to(hyrax.admin_appearance_path(locale: 'en'))
+            expect(flash[:notice]).to include('The appearance was successfully updated')
+            expect(Site.instance.default_work_image?).to be true
+          end
+
+          it 'enqueues one jobs' do
+            expect(ReindexWorksJob).to receive(:perform_later).once
+
+            f = fixture_file_upload('/images/nypl-hydra-of-lerna.jpg', 'image/jpg')
+            post :update, params: { admin_appearance: { default_work_image: f } }
+          end
+        end
+
         it "redirects to the site" do
-          skip
           put :update, params: { admin_appearance: valid_attributes }
           expect(response).to redirect_to(hyrax.admin_appearance_path(locale: 'en'))
         end
@@ -75,7 +124,6 @@ RSpec.describe Hyrax::Admin::AppearancesController, type: :controller, singleten
         end
 
         it "re-renders the 'show' template" do
-          skip
           put :update, params: { admin_appearance: invalid_attributes }
           expect(response).to redirect_to(action: "show")
         end

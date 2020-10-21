0f2c8659c1a3ca70ef8401398863a83c9a564606
Rob Kaufman <rob@notch8.com>
2020-10-21 03:54:11 +0000

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_hyku_session', domain: ".#{ENV['SETTINGS__MULTITENANCY__ROOT_HOST']}"
+if Rails.env.test?
+  Rails.application.config.session_store :cookie_store, key: '_hyku_session'
+else
+  Rails.application.config.session_store :cookie_store, key: '_hyku_session', domain: ".#{ENV['SETTINGS__MULTITENANCY__ROOT_HOST']}"
+end

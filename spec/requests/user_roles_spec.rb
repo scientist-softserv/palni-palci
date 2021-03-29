00f8633dedee9aa8c580633f4588ca42b50dba27
JP Engstrom <jpengstrom@MacBook-Pro.local>
2021-03-29 10:10:15 -0700

    user roles request specs

--- spec/requests/user_roles_spec.rb
+++ spec/requests/user_roles_spec.rb
@@ -4,20 +4,8 @@ RSpec.describe "User roles", type: :request, clean: true, multitenant: true do
   let(:account) { create(:account) }
   let(:tenant_user_attributes) { attributes_for(:user) }
 
-  before do
-    WebMock.disable!
-    Apartment::Tenant.create(account.tenant)
-    Apartment::Tenant.switch(account.tenant) do
-      Site.update(account: account)
-    end
-  end
-
-  after do
-    WebMock.enable!
-    Apartment::Tenant.drop(account.tenant)
-  end
-
   context 'an unregistered user' do
+
     let(:user_params) do 
       { 
         user: {
@@ -28,6 +16,19 @@ RSpec.describe "User roles", type: :request, clean: true, multitenant: true do
       }
     end
 
+    before do
+      WebMock.disable!
+      Apartment::Tenant.create(account.tenant)
+      Apartment::Tenant.switch(account.tenant) do
+        Site.update(account: account)
+      end
+    end
+  
+    after do
+      WebMock.enable!
+      Apartment::Tenant.drop(account.tenant)
+    end
+
     it 'can sign up' do
       expect { post "http://#{account.cname}/users", params: user_params }
         .to change(User, :count).by(1)
@@ -36,6 +37,74 @@ RSpec.describe "User roles", type: :request, clean: true, multitenant: true do
     end
   end
 
-  # try to go to the user index url's with certain roles. no permission: 302. 200 == success. cmk + k to clear and look at logs
+  context 'a superadmin user' do
+    let(:superadmin) { FactoryBot.create(:superadmin) }
+
+    before do
+      login_as(superadmin, scope: :user)
+    end
+
+    it 'can access the users index' do
+      get proprietor_users_url
+      expect(response.status).to eq(200)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  context 'a registered user with user_admin role' do
+    let(:user_admin) { FactoryBot.create(:user_admin) }
+
+    before do
+      login_as(user_admin, scope: :user)
+    end
+
+    it 'can access the users index' do
+      get proprietor_users_url
+      expect(response.status).to eq(200)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  context 'a registered user with user_manager role' do
+    let(:user_manager) { FactoryBot.create(:user_manager) }
+
+    before do
+      login_as(user_manager, scope: :user)
+    end
+
+    it 'can access the users index' do
+      get proprietor_users_url
+      expect(response.status).to eq(200)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  context 'a registered user with user_reader role' do
+    let(:user_reader) { FactoryBot.create(:user_reader) }
+
+    before do
+      login_as(user_reader, scope: :user)
+    end
+
+    it 'can access the users index' do
+      get proprietor_users_url
+      expect(response.status).to eq(200)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  context 'a registered user with no role' do
+    let(:user) { FactoryBot.create(:user) }
+
+    before do
+      login_as(user, scope: :user)
+    end
+
+    it 'cannot access the users index' do
+      get proprietor_users_url
+      expect(response.status).to eq(302)
+      expect(response).to have_http_status(:redirect)
+    end
+  end
 
 end

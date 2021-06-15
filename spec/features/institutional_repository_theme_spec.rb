fd245a77baa9d3b154481ed5fc5293165e72cd4a
Lea Ann Bradford <leaann@notch8.com>
2021-06-15 17:28:43 +0000

    fix homepage stats

--- spec/features/institutional_repository_theme_spec.rb
+++ spec/features/institutional_repository_theme_spec.rb
@@ -10,10 +10,11 @@ RSpec.describe 'Admin can select institutional repository theme', type: :feature
     create(:generic_work,
            title: ['Llamas and Alpacas'],
            keyword: ['llama', 'alpaca'],
+           resource_type: ['Software or Program Code'],
            user: user)
   end
 
-  context "as a repository admin" do
+  context 'as a repository admin' do
     it 'sets the institutional repository theme when the theme form is saved' do
       login_as admin
       visit 'admin/appearance'
@@ -44,7 +45,7 @@ RSpec.describe 'Admin can select institutional repository theme', type: :feature
       visit '/'
       expect(page).to have_css('body.institutional_repository')
       expect(page).to have_css('nav.navbar.navbar-inverse.navbar-static-top.institutional-repsitory-nav')
-      expect(page).to have_css('div.institutional-repository-carousel')
+      expect(page).to have_css('div.ir-stats')
       expect(page).to have_css('div.institutional-repository-featured-researcher')
       expect(page).to have_css('div.institutional-repository-recent-uploads')
       expect(page).to have_css('div.col-xs-12.col-md-8.institutional-repository.collections-container')
@@ -52,6 +53,22 @@ RSpec.describe 'Admin can select institutional repository theme', type: :feature
       expect(page).not_to have_css('ul.nav.nav-pills')
       expect(page).not_to have_css('nav.navbar.navbar-inverse.navbar-static-top.cultural-repository-nav')
       expect(page).not_to have_css('background-container-gradient')
+    end
+
+    it 'renders the stats carousel if there are more than 6 resource_types' do
+      work.resource_type = ['Article', 'Audio', 'Book', 'Capstone Project', 'Conference Proceeding', 'Dataset', 'Software or Program Code']
+      work.save
+      login_as admin
+      visit '/admin/appearance'
+      click_link('Themes')
+      select('Institutional Repository', from: 'Home Page Theme')
+      find('body').click
+      click_on('Save')
+      site = Site.last
+      account.sites << site
+      allow_any_instance_of(ApplicationController).to receive(:current_account).and_return(account)
+      visit '/'
+      expect(page).to have_css('div.institutional-repository-carousel')
     end
   end
 end

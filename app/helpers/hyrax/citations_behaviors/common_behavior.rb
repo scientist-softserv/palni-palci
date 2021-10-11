2a98ac66a71692d9742cff1d40ac6c090b9b02b3
Sara G <geezy@Notch8-MacBook-Pro.local>
2021-10-11 13:36:57 -0500

    citations url

--- app/helpers/hyrax/citations_behaviors/common_behavior.rb
+++ app/helpers/hyrax/citations_behaviors/common_behavior.rb
@@ -3,9 +3,7 @@
 module Hyrax
   module CitationsBehaviors
     module CommonBehavior
-      def persistent_url(work)
-        "#{Hyrax.config.persistent_hostpath}#{work.id}"
-      end
+      def persistent_url(work); end
 
       def clean_end_punctuation(text)
         return text[0, text.length - 1] if text && ([".", ",", ":", ";", "/"].include? text[-1, 1])

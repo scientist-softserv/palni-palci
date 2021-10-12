332867828e9a84c4c6b3a8a69ab9484b43ef0308
Sara G <its.sara.geezy@gmail.com>
2021-10-12 12:37:51 +0000

    sort numbers

--- app/models/sort_title.rb
+++ app/models/sort_title.rb
@@ -6,8 +6,16 @@ class SortTitle
   end
 
   def alphabetical
-    title = @title.titlecase
-    title = title.sub(/^An/, '').sub(/^A/, '').sub(/^The/, '')
+    title = @title.downcase
+    title = title.sub(/^an/, '').sub(/^a/, '').sub(/^the/, '')
+    title_elements = title.split(' ')
+    new_title = []
+    title_elements.each do |str|
+      numbers = str.gsub(/[^\d]/, '')
+      str = numbers.rjust(6, '0') unless numbers.empty?
+      new_title.push(str)
+    end
+    title = new_title.join(' ')
     title.strip
   end
 end

70f96d4ed024f47edc49a53c2f907f759b2f0bd3
April Rieger <april.rieger@yahoo.com>
2022-12-06 14:21:27 -0800

    remove png, only captcha the email

--- app/controllers/hyrax/contact_form_controller.rb
+++ app/controllers/hyrax/contact_form_controller.rb
@@ -123,10 +123,10 @@ module Hyrax
       def setup_negative_captcha
         @captcha = NegativeCaptcha.new(
           # A secret key entered in environment.rb. 'rake secret' will give you a good one.
-          secret: ENV.fetch('NEGATIVE_CAPTCHA_SECRET'),
+          secret: ENV.fetch('NEGATIVE_CAPTCHA_SECRET', 'default-value-change-me'),
           spinner: request.remote_ip,
           # Whatever fields are in your form
-          fields: [:name, :email, :message],
+          fields: [:email],
           # If you wish to override the default CSS styles (position: absolute; left: -2000px;) used to position the fields off-screen
           css: "display: none",
           params: params

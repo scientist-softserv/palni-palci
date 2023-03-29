87b2e39a1fe3173fff8fb87fafe58333c330532c
Summer Cook <summer@scientist.com>
2023-03-29 15:23:50 -0500

    rubocop

--- app/renderers/hyrax/renderers/attribute_renderer_decorator.rb
+++ app/renderers/hyrax/renderers/attribute_renderer_decorator.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
+
 # OVERRIDE Hyrax v3.4.2 Enable markdown rendering on work show page metadata
 
-module Hyrax::Renderers
-  module AttributeRendererDecorator
-    include ApplicationHelper
+module Hyrax
+  module Renderers
+    module AttributeRendererDecorator
+      include ApplicationHelper
 
-    private
-    def attribute_value_to_html(value)
-      if microdata_value_attributes(field).present?
-        "<span#{html_attributes(microdata_value_attributes(field))}>#{markdown(li_value(value))}</span>"
-      else
-        markdown(li_value(value))
-      end
+      private
+
+        def attribute_value_to_html(value)
+          if microdata_value_attributes(field).present?
+            "<span#{html_attributes(microdata_value_attributes(field))}>#{markdown(li_value(value))}</span>"
+          else
+            markdown(li_value(value))
+          end
+        end
     end
   end
 end

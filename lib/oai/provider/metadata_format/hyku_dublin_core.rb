61e415dc04ddea3356197c1100d445297dff36e9
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2023-04-06 10:43:28 -0700

    OAI - add links to public files on works (#440)
    * add file_url tags to custom oai_hyku prefix
    
    * skip blank values, reorder some fields
    
    * extract adding of file_urls to its own method
    
    * add spec coverage for custom oai_hyku metadata prefix
    
    * resolve rubocop warnings
    
    * consolidate public file set id fetching logic

--- lib/oai/provider/metadata_format/hyku_dublin_core.rb
+++ lib/oai/provider/metadata_format/hyku_dublin_core.rb
@@ -13,13 +13,13 @@ module OAI
           # Dublin Core Terms Fields
           # For new fields, add here first then add to #map_oai_hyku
           @fields = %i[
-            abstract access_right accessibility_feature accessibility_hazard accessibility_summary
-            additional_information advisor alternate_version_id alternative_title audience based_near
-            bibliographic_citation committee_member contributor creator date_created degree_discipline
-            degree_grantor degree_level degree_name department description discipline education_level
-            extent format has_model identifier keyword language learning_resource_type license newer_version_id
-            oer_size previous_version_id publisher related_item_id related_url resource_type rights_holder
-            rights_notes rights_statement source subject table_of_contents title
+            identifier title abstract access_right accessibility_feature accessibility_hazard
+            accessibility_summary additional_information advisor alternate_version_id alternative_title
+            audience based_near bibliographic_citation committee_member contributor creator date_created
+            degree_discipline degree_grantor degree_level degree_name department description discipline
+            education_level extent format has_model keyword language learning_resource_type license
+            newer_version_id oer_size previous_version_id publisher related_item_id related_url
+            resource_type rights_holder rights_notes rights_statement source subject table_of_contents
           ]
         end
 
@@ -30,6 +30,8 @@ module OAI
           xml.tag!(prefix.to_s) do
             fields.each do |field|
               values = value_for(field, record, map)
+              next if values.blank?
+
               if values.respond_to?(:each)
                 values.each do |value|
                   xml.tag! field.to_s, value
@@ -38,8 +40,28 @@ module OAI
                 xml.tag! field.to_s, values
               end
             end
+            add_public_file_urls(xml, record)
           end
           xml.target!
+        end
+
+        def add_public_file_urls(xml, record)
+          return if record[:file_set_ids_ssim].blank?
+
+          fs_ids = record[:file_set_ids_ssim].join('" OR "')
+          public_fs_ids = ActiveFedora::SolrService.query(
+            "id:(\"#{fs_ids}\") AND " \
+            "has_model_ssim:FileSet AND " \
+            "visibility_ssi:#{Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC}",
+            fl: ["id"],
+            rows: 1024 # maximum
+          )
+          return if public_fs_ids.blank?
+
+          public_fs_ids.each do |fs_id_hash|
+            file_download_path = "https://#{Site.instance.account.cname}/downloads/#{fs_id_hash['id']}"
+            xml.tag! 'file_url', file_download_path
+          end
         end
 
         def header_specification

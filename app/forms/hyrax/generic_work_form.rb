d094be9b915c6c77e7f9b3616d307dd2f33169c7
April Rieger <april.rieger@yahoo.com>
2023-04-29 12:25:20 -0700

    add additional rights field -- still need to map to client picked dc terms to map to, currently mapped to accessRights and it's showing up in the access rights field

--- app/forms/hyrax/generic_work_form.rb
+++ app/forms/hyrax/generic_work_form.rb
@@ -7,7 +7,7 @@ module Hyrax
     include Hyrax::FormTerms
     self.model_class = ::GenericWork
     include HydraEditor::Form::Permissions
-    self.terms += %i[resource_type format institution types]
+    self.terms += %i[resource_type format institution types additional_rights_information]
     # remove default terms, self.terms -= %i[one two three] -= [single]
     self.required_fields += %i[title creator rights_statement date_created resource_type institution types]
     # remove default required terms, self.required_fields -= %i[one two three]  -= [single]

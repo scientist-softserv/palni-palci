7a737d6a7d2ba3b9943bfe4bab5b88b01cf85141
Summer Cook <summer@scientist.com>
2023-05-09 11:45:20 -0500

    rubocop

--- app/models/paper_or_report.rb
+++ app/models/paper_or_report.rb
@@ -15,12 +15,12 @@ class PaperOrReport < ActiveFedora::Base
   validates :institution, presence: { message: 'Your work must have an institution.' }
   validates :creator, presence: { message: 'Your work must have a creator.' }
   validates :video_embed,
-  format: {
-    # regex matches only youtube & vimeo urls that are formatted as embed links.
-    with: /(http:\/\/|https:\/\/)(www\.)?(player\.vimeo\.com|youtube\.com\/embed)/,
-    message: "Error: must be a valid YouTube or Vimeo Embed URL."
-  },
-  if: :video_embed?
+            format: {
+              # regex matches only youtube & vimeo urls that are formatted as embed links.
+              with: /(http:\/\/|https:\/\/)(www\.)?(player\.vimeo\.com|youtube\.com\/embed)/,
+              message: "Error: must be a valid YouTube or Vimeo Embed URL."
+            },
+            if: :video_embed?
 
   property :institution, predicate: ::RDF::Vocab::ORG.organization, multiple: false do |index|
     index.as :stored_searchable, :facetable
@@ -93,7 +93,6 @@ class PaperOrReport < ActiveFedora::Base
   def video_embed?
     video_embed.present?
   end
-
 
   # This must be included at the end, because it finalizes the metadata
   # schema (by adding accepts_nested_attributes)

927f256beafd77042d3157dd254fda607d658259
Alisha Evans <alisha@scientist.com>
2023-05-10 13:16:03 -0500

    449 color labels (#452)
    * update the default color labels and add hint text
    
    * show the hints on the colors form
    
    * update a spec with the new color name
    
    * fix styling for the color form with hints
    
    * hide the active_tabs_background_color color picker from the form because it does not seem to be doing anything

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,7 @@ module ApplicationHelper
   def hint_for(term:, record_class: nil)
     hint = locale_for(type: 'hints', term: term, record_class: record_class)
 
-    return hint unless hint.include?('translation missing')
+    return hint unless missing_translation(hint)
   end
 
   def locale_for(type:, term:, record_class:)
@@ -22,9 +22,13 @@ module ApplicationHelper
     default_locale     = t("simple_form.#{type}.#{work_or_collection}.#{@term}").html_safe
     locale             = t("hyrax.#{@record_class}.#{type}.#{@term}").html_safe
 
-    return default_locale if locale.include?('translation missing')
+    return default_locale if missing_translation(locale)
 
     locale
+  end
+
+  def missing_translation(value)
+    value.include?('translation missing')
   end
 
   def markdown(text)

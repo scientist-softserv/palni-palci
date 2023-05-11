782b0ae1fd5dfd1a571d3446f016253c6da65472
Summer Cook <summer@scientist.com>
2023-05-11 18:17:13 -0500

    fix services to bring wrok forms back and fix specs

--- app/services/hyrax/resource_type_service.rb
+++ app/services/hyrax/resource_type_service.rb
@@ -3,8 +3,21 @@
 module Hyrax
   # Provide select options for the types field
   class ResourceTypeService < QaSelectService
+    mattr_accessor :authority
+    self.authority = Qa::Authorities::Local.subauthority_for('resource_types')
+
     def initialize(_authority_name = nil)
       super('resource_types')
+    end
+
+    def self.select_options
+      authority.all.map do |element|
+        [element[:label], element[:id]]
+      end
+    end
+
+    def self.label(id)
+      authority.find(id).fetch('term')
     end
   end
 end
--- app/services/hyrax/types_service.rb
+++ app/services/hyrax/types_service.rb
@@ -3,8 +3,20 @@
 module Hyrax
   # Provide select options for the types field
   class TypesService < QaSelectService
+    mattr_accessor :authority
+    self.authority = Qa::Authorities::Local.subauthority_for('types')
     def initialize(_authority_name = nil)
       super('types')
+    end
+
+    def self.select_options
+      authority.all.map do |element|
+        [element[:label], element[:id]]
+      end
+    end
+
+    def self.label(id)
+      authority.find(id).fetch('term')
     end
   end
 end

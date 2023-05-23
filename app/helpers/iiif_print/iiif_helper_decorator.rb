db1cc6f9b971dfebc9c790b3080103842b76cf4b
braydonjustice <braydonjustice@gmail.com>
2023-05-23 12:43:33 -0700

    account for iiifprint

--- app/helpers/iiif_print/iiif_helper_decorator.rb
+++ app/helpers/iiif_print/iiif_helper_decorator.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 #
 # OVERRIDE: Hyrax hyrax-v3.5.0 to add download restricted specific uv-config
-module Hyrax
-  module IiifHelperDecorator
+module IiifPrint
+  module IiifHelperDecoratorDecorator
     def universal_viewer_config_url
       if Site.account.settings[:allow_downloads].nil? || Site.account.settings[:allow_downloads].to_i.nonzero?
-        "#{request&.base_url}/uv/uv-config.json"
+        "#{request&.base_url}#{IiifPrint.config.uv_config_path}"
       else
         "#{request&.base_url}/uv/uv-config-no-download.json"
       end
@@ -13,4 +13,4 @@ module Hyrax
   end
 end
 
-Hyrax::IiifHelper.prepend(Hyrax::IiifHelperDecorator)
+IiifPrint::IiifHelperDecorator.prepend(IiifPrint::IiifHelperDecoratorDecorator)

c83ea858bb0dfe5ae7818536daf6bb2d4e445e50
Summer <73361970+summer-cook@users.noreply.github.com>
2023-05-24 14:54:11 -0500

    CDL Worktype (#501)
    * remove syntax error commas, inherit the presenter from the right place, and index abstracts same as generic work
    
    * add custom field for chronology note
    
    * update catalog controller and add custom attribute_rows for cdls
    
    * needed to use iiif_viewer instead of universal_viewer method on the scholarly show page, or else an error was thrown
    
    * fix attribute rows file name and cdl presenter
    
    * add to solr document, fix attribute rows
    
    * rubocop
    
    * one more rubocop fix
    
    * feedback from PR

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.5'
--- app/forms/hyrax/cdl_form.rb
+++ app/forms/hyrax/cdl_form.rb
@@ -8,7 +8,7 @@ module Hyrax
     include HydraEditor::Form::Permissions
     self.terms.prepend(:admin_note).uniq! # rubocop:disable Style/RedundantSelf
     self.terms += %i[resource_type additional_information bibliographic_citation contributing_library library_catalog_identifier chronology_note]
-    self.terms -=%i[based_near]
+    self.terms -= %i[based_near]
     self.required_fields = %i[title creator keyword rights_statement resource_type]
   end
 end

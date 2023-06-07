6673a964ca45ae33492ccf916b54319c2518113d
Summer Cook <summer@scientist.com>
2023-06-07 11:31:29 -0500

    update wording

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,8 +71,8 @@ if ENV['CHROME_HOSTNAME'].present?
   )
 
   Capybara.register_driver :chrome do |app|
-    # Comment this in to run selenium tests with M1 Machines
-    WebMock.allow_net_connect!
+    # Uncomment this to run selenium tests with M1 Machines
+   # WebMock.allow_net_connect!
     d = Capybara::Selenium::Driver.new(app,
                                        browser: :remote,
                                        desired_capabilities: capabilities,

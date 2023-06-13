23769562f16ece6f807359773623c4398c12cb10
Summer Cook <summer@scientist.com>
2023-06-13 14:34:59 -0500

    update indexers to all inherit from AppIndexer, add blacklight_range_limit gem & configure

--- app/indexers/etd_indexer.rb
+++ app/indexers/etd_indexer.rb
@@ -2,20 +2,11 @@
 
 # Generated via
 #  `rails generate hyrax:work Etd`
-class EtdIndexer < Hyrax::WorkIndexer
-  # This indexes the default metadata. You can remove it if you want to
-  # provide your own metadata and indexing.
-  include Hyrax::IndexesBasicMetadata
-
-  # Fetch remote labels for based_near. You can remove this if you don't want
-  # this behavior
-  include Hyrax::IndexesLinkedMetadata
+class EtdIndexer < AppIndexer
 
   # Uncomment this block if you want to add custom indexing behavior:
   def generate_solr_document
     super.tap do |solr_doc|
-      solr_doc['bulkrax_identifier_tesim'] = object.bulkrax_identifier
-      solr_doc['title_ssi'] = SortTitle.new(object.title.first).alphabetical
       solr_doc['admin_note_tesim'] = object.admin_note
     end
   end

a4baf0db813c6b3557a515756e31c4eb5eae5ebc
Summer Cook <summer@scientist.com>
2023-06-23 14:00:17 -0300

    remove unnecessary comment

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class SearchBuilder < Blacklight::SearchBuilder
-  # self.default_processor_chain += [:add_advanced_parse_q_to_solr, :add_advanced_search_to_solr]
   include Blacklight::Solr::SearchBuilderBehavior
   include BlacklightAdvancedSearch::AdvancedSearchBuilder
   include BlacklightRangeLimit::RangeLimitBuilder

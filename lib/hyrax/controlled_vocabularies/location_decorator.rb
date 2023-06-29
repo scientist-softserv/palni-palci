a574173f6bd9032ee747aa4e0d608a60a66baeca
Summer Cook <summer@scientist.com>
2023-06-29 11:15:43 -0300

    update comments

--- lib/hyrax/controlled_vocabularies/location_decorator.rb
+++ lib/hyrax/controlled_vocabularies/location_decorator.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
-
-# TODO get this decorator to work, and remove the location.rb override.
 module Hyrax
   module LocationDecorator
+    # the location controlled vocabulary throws an error that split is undefined, which causes 500 errors for works with their Location field filled in.
+    # See https://github.com/scientist-softserv/palni-palci/issues/524 for more details
     def split(argument1, argument2)
       []
     end

cca5efb55b11f4acf30b8def3b7868aa6920cd44
Summer Cook <summer@scientist.com>
2023-06-29 12:27:35 -0300

    fix comment!

--- lib/hyrax/controlled_vocabularies/location_decorator.rb
+++ lib/hyrax/controlled_vocabularies/location_decorator.rb
@@ -5,7 +5,7 @@ module Hyrax
     # the Location class throws an error that split is undefined since it is an enumerator, but this error is coming from deeply nested dependencies.
     # This causes 500 errors for works with their Location field filled in.
     # defining split just for the purpose of getting past this error seemed to work and allow locations to save correctly with no adverse effects.
-    # See https://github.com/scientist-softserv/palni-palci/issues/524 for more details
+    # See https://github.com/scientist-softserv/palni-palci/issues/530 for more details
     def split(*)
       []
     end

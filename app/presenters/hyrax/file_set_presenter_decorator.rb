7eb1b6aab38e27487d59673f5d903e5d3f5867b1
Christy Karpinski <christykarpinski@Christy23.local>
2023-07-06 17:30:36 -0500

    ran rubocop and auto corrected errors

--- app/presenters/hyrax/file_set_presenter_decorator.rb
+++ app/presenters/hyrax/file_set_presenter_decorator.rb
@@ -3,11 +3,11 @@
 # OVERRIDE Hyrax v3.5.0 to change what appears in the title tags for pages
 
 module Hyrax
-     module FileSetPresenterDecorator
-          def page_title
-               "#{title.first} | #{I18n.t('hyrax.product_name')} | ID: #{id}"
-          end
-     end
+  module FileSetPresenterDecorator
+    def page_title
+      "#{title.first} | #{I18n.t('hyrax.product_name')} | ID: #{id}"
+    end
+  end
 end
 
 Hyrax::FileSetPresenter.prepend(Hyrax::FileSetPresenterDecorator)

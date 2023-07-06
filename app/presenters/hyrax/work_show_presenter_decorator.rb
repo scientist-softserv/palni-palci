0f3923bfda5d2779a88ce7eb70943ed7f12284ba
Christy Karpinski <christykarpinski@Christy23.local>
2023-07-06 17:12:26 -0500

    using decorator file to override title tag for work show pages

---
+++ app/presenters/hyrax/work_show_presenter_decorator.rb
@@ -0,0 +1,12 @@
+
+# OVERRIDE Hyrax v3.5.0 to change what appears in the title tags for pages
+
+module Hyrax
+     module WorkShowPresenterDecorator
+          def page_title
+               "#{title.first} | #{I18n.t('hyrax.product_name')} | ID: #{id}"
+          end
+     end
+end
+
+Hyrax::WorkShowPresenter.prepend(Hyrax::WorkShowPresenterDecorator)

5d655d2f685e0483171b9b69a82f1d7d7c7e67b0
Lea Ann Bradford <ltrammer@gmail.com>
2023-07-24 09:51:55 -0700

    add feature flipper for auth provider tab (#629)

--- config/features.rb
+++ config/features.rb
@@ -20,4 +20,8 @@ Flipflop.configure do
   feature :show_recently_uploaded,
           default: true,
           description: "Shows the Recently Uploaded tab on the homepage."
+
+  feature :show_auth_provider_in_admin_dashboard,
+          default: false,
+          description: "Shows the Auth Provider tab on the admin dashboard."
 end

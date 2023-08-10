3455ef299841cacf6800b5bb865cf769bae9dd23
leaannbradford <ltrammer@gmail.com>
2023-08-10 12:56:50 -0700

    delete auth provider code, put identity provider behind feature flipper

--- spec/factories/auth_providers.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :auth_provider, class: 'AuthProvider' do
-    provider { 'saml' }
-    client_id { 'client_id' }
-    client_secret { 'client_secret' }
-    idp_sso_service_url { 'idp_sso_service_url' }
-    account_id { 1 }
-  end
-end

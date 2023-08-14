860e65f0c33ba097c7b0e2b6c1fc71e6a87aaa56
Summer Cook <summer@scientist.com>
2023-08-14 13:35:11 -0300

    fix spec

--- spec/models/sushi/platform_report_spec.rb
+++ spec/models/sushi/platform_report_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal:true
 
 RSpec.describe Sushi::PlatformReport do
-  let(:account) { double(Account, institution_name: 'Pitt', institution_id_data: {}) }
+  let(:account) { double(Account, institution_name: 'Pitt', institution_id_data: {}, cname: 'pitt.hyku.test') }
 
   describe '#to_hash' do
     before { create_hyrax_countermetric_objects }

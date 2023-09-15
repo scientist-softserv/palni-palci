88c256b5cad7093b5bb6ac79fd7963a7de732358
Shana Moore <shana@scientist.com>
2023-09-15 07:38:44 -0700

    🧹 add yardoc comments to advancedSearchHelper module
    The yard doc comments explain the purpose and function of each method, as well as the expected types of the parameters and return values. Note that the fetch_service_for method was also marked as private, as it is an internal helper method not intended for external use.

--- spec/helpers/blacklight/advanced_search_helper_spec.rb
+++ spec/helpers/blacklight/advanced_search_helper_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Blacklight::AdvancedSearchHelper do
     end
 
     it "returns the client specified advanced search fields" do
-      expect(primary_search_fields_for(search_fields_for_advanced_search).map{|search| search.last.key}).to eq(["title", "creator", "date_created", "keyword", "license", "subject"])
+      expect(primary_search_fields_for(search_fields_for_advanced_search).map { |search| search.last.key }).to eq(["title", "creator", "date_created", "keyword", "license", "subject"])
     end
   end
 
@@ -27,20 +27,20 @@ RSpec.describe Blacklight::AdvancedSearchHelper do
     context 'when key is valid' do
       it "checks if the key exists in QA::Authorities::Local.names" do
         valid_local_authorities = search_fields_for_advanced_search.select { |key, _value| local_authority?(key) }
-        
+
         valid_local_authorities.each do |key, _value|
           expect(local_authority?(key)).to be true
-        end 
+        end
       end
     end
 
     context 'when the key is invalid' do
       it "checks if the key exists in QA::Authorities::Local.names" do
-        invalid_local_authorities = search_fields_for_advanced_search.select { |key, _value| !local_authority?(key) }
-        
+        invalid_local_authorities = search_fields_for_advanced_search.reject { |key, _value| local_authority?(key) }
+
         invalid_local_authorities.each do |key, _value|
           expect(local_authority?(key)).to be false
-        end 
+        end
       end
     end
   end
@@ -51,19 +51,17 @@ RSpec.describe Blacklight::AdvancedSearchHelper do
 
       valid_local_authority_keys.each do |key|
         local_authority_terms = fetch_local_yml(key)['terms'].map { |term| [term['term'], term['id']] }
-  
-        expect(options_for_qa_select(key)).to eq(local_authority_terms) 
+
+        expect(options_for_qa_select(key)).to eq(local_authority_terms)
       end
     end
   end
 
   # a utility method for finding the correct yml files because the naming is inconsistent, plural vs singular
   def fetch_local_yml(key)
-    begin
-      YAML.load_file("config/authorities/#{key}.yml")
-    rescue Errno::ENOENT
-      YAML.load_file("config/authorities/#{key.pluralize}.yml")
-    end
+    YAML.load_file("config/authorities/#{key}.yml")
+  rescue Errno::ENOENT
+    YAML.load_file("config/authorities/#{key.pluralize}.yml")
   end
 
   describe "#fetch_service_for" do

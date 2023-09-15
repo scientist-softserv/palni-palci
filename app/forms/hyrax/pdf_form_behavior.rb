bbcf03cd962bfab916a8f17f3ba15d5dad7077d8
Kirk Wang <kirk.wang@scientist.com>
2023-09-14 20:18:01 -0700

    🐛 Remove PDF.js related fields from form
    The `show_pdf_viewer` and `show_pdf_download_button` fields were showing
    up on the form so this commit will remove them.

--- app/forms/hyrax/pdf_form_behavior.rb
+++ app/forms/hyrax/pdf_form_behavior.rb
@@ -5,7 +5,14 @@ module Hyrax
     extend ActiveSupport::Concern
 
     included do
+      class_attribute :hidden_terms
+
       self.terms += %i[show_pdf_viewer show_pdf_download_button]
+      self.hidden_terms = %i[show_pdf_viewer show_pdf_download_button]
+    end
+
+    def hidden?(key)
+      hidden_terms.include? key.to_sym
     end
   end
 end

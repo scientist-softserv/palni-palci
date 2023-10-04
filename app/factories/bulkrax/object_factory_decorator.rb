26ae01378bd9c1317869aa50964c741420076326
Kirk Wang <kirk.wang@scientist.com>
2023-10-04 11:52:29 -0700

    🐛 Bring fix for entry show page not showing link
    This commit will bring in a fix from Bulkrax that correctly shows the
    object from the entry show page.
    
    ref:
      - https://github.com/samvera-labs/bulkrax/commit/0e68a5ea81307dc9e06f9a1e7c0c4cc8cd3de873

--- app/factories/bulkrax/object_factory_decorator.rb
+++ app/factories/bulkrax/object_factory_decorator.rb
@@ -8,6 +8,21 @@ module Bulkrax
     def environment(attrs)
       Hyrax::Actors::Environment.new(object, Ability.new(@user), attrs, true)
     end
+
+    # TODO: Verify and remove this once Bulkrax is updated 5.4.1 over greater and this is no longer needed
+    def search_by_identifier
+      # ref: https://github.com/samvera-labs/bulkrax/issues/866
+      # ref:https://github.com/samvera-labs/bulkrax/issues/867
+      # work_index = ::ActiveFedora.index_field_mapper.solr_name(work_identifier, :facetable)
+      work_index = work_identifier
+      query = { work_index =>
+                source_identifier_value }
+      # Query can return partial matches (something6 matches both something6 and something68)
+      # so we need to weed out any that are not the correct full match. But other items might be
+      # in the multivalued field, so we have to go through them one at a time.
+      match = klass.where(query).detect { |m| m.send(work_identifier).include?(source_identifier_value) }
+      return match if match
+    end
   end
 end
 

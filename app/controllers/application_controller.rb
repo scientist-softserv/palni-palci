9827c14489eedde7b5fffe6642bb038fb3aa4fd8
Kirk Wang <kirk.wang@scientist.com>
2023-10-06 22:25:02 -0700

    Revert "🐛 Mixin `HykuKnapsack::ApplicationHelper`"
    This reverts commit ae093f1c04e036057c17ec14e8a8ebeeac32a6bb.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@
 
 class ApplicationController < ActionController::Base
   include HykuHelper
-  include HykuKnapsack::ApplicationHelper
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception, prepend: true

a65e75951c580bb25d0105efa965814d672996cb
Shana Moore <shana@scientist.com>
2023-10-31 16:12:19 -0700

    🐛 add limited reindex to collections job
    Add limited reindex to the collection_id  path and refactor in honor of 'Don't repeat yourself'.
    Before this commit calling this job from a rake task took hours to complete.
    It may be because of nested indexing. This change will ensure limited reindexing
    is enforced regardless of the logical branch

--- app/jobs/reindex_collections_job.rb
+++ app/jobs/reindex_collections_job.rb
@@ -3,13 +3,16 @@
 class ReindexCollectionsJob < ApplicationJob
   def perform(collection_id = nil)
     if collection_id.present?
-      collection = Collection.find(collection_id)
-      collection&.update_index
+      update_collection_index(Collection.find(collection_id))
     else
-      Collection.find_each do |collection|
-        collection.try(:reindex_extent=, Hyrax::Adapters::NestingIndexAdapter::LIMITED_REINDEX)
-        collection.update_index
-      end
+      Collection.find_each { |collection| update_collection_index(collection) }
     end
+  end
+
+  private
+
+  def update_collection_index(collection)
+    collection.try(:reindex_extent=, Hyrax::Adapters::NestingIndexAdapter::LIMITED_REINDEX)
+    collection&.update_index
   end
 end

aa2f3cb9a3290e7d2990b10415d71cc4b0cb075e
Shana Moore <shana@scientist.com>
2023-11-01 09:31:22 -0700

    ✅ Add spec for ReindexCollectionsJob

---
+++ spec/jobs/reindex_collections_job_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe ReindexCollectionsJob, type: :job do
+  describe '#perform' do
+    context 'when collection_id is present' do
+      it 'updates the index of the specified collection' do
+        collection = create(:collection)
+        allow_any_instance_of(Collection).to receive(:update_index)
+        described_class.perform_now(collection.id)
+      end
+    end
+
+    context 'when collection_id is not present' do
+      it 'updates the index of all collections' do
+        create_list(:collection, 3)
+        allow_any_instance_of(Collection).to receive(:update_index)
+        described_class.perform_now
+      end
+    end
+  end
+end

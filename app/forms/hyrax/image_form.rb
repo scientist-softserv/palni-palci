a9b2399b560aed9ee66892b8974c9f535d950c87
Shana Moore <shana@scientist.com>
2023-12-06 10:51:23 -0800

    🎁 Video Embed: Image
    Adds video embed to CDL work type.
    
    Issue:
    - #911

--- app/forms/hyrax/image_form.rb
+++ app/forms/hyrax/image_form.rb
@@ -8,8 +8,12 @@ module Hyrax
     self.model_class = ::Image
     include PdfFormBehavior
 
-    self.terms = [:admin_note] + self.terms # rubocop:disable Style/RedundantSelf
+    self.terms = [:admin_note, :video_embed] + self.terms # rubocop:disable Style/RedundantSelf
     self.terms += %i[resource_type extent additional_information bibliographic_citation]
     self.required_fields = %i[title creator keyword rights_statement resource_type]
+
+    def secondary_terms
+      super + %i[video_embed]
+    end
   end
 end

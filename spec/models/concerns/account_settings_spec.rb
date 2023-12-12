8dabf5f5f6e21fe22ea8c65d11e8ca9b3b937dd7
LaRita Robinson <larita@scientist.com>
2023-12-12 15:14:36 -0500

    🧹 Fix specs for removal of tenant oauth settings
    Removed via https://github.com/scientist-softserv/palni-palci/issues/910

--- spec/models/concerns/account_settings_spec.rb
+++ spec/models/concerns/account_settings_spec.rb
@@ -22,12 +22,6 @@ RSpec.describe AccountSettings do
                                                                                 file_size_limit
                                                                                 geonames_username
                                                                                 google_analytics_id
-                                                                                google_oauth_app_name
-                                                                                google_oauth_app_version
-                                                                                google_oauth_client_email
-                                                                                google_oauth_private_key_path
-                                                                                google_oauth_private_key_secret
-                                                                                google_oauth_private_key_value
                                                                                 gtm_id
                                                                                 oai_admin_email
                                                                                 oai_prefix
@@ -36,7 +30,7 @@ RSpec.describe AccountSettings do
                                                                                 smtp_settings
                                                                                 solr_collection_options
                                                                                 ssl_configured]
-        expect(account.public_settings(is_superadmin: true).size).to eq 29
+        expect(account.public_settings(is_superadmin: true).size).to eq 23
       end
       # rubocop:enable RSpec/ExampleLength
     end
@@ -56,18 +50,12 @@ RSpec.describe AccountSettings do
                                                                                  email_subject_prefix
                                                                                  geonames_username
                                                                                  google_analytics_id
-                                                                                 google_oauth_app_name
-                                                                                 google_oauth_app_version
-                                                                                 google_oauth_client_email
-                                                                                 google_oauth_private_key_path
-                                                                                 google_oauth_private_key_secret
-                                                                                 google_oauth_private_key_value
                                                                                  gtm_id
                                                                                  oai_admin_email
                                                                                  smtp_settings
                                                                                  solr_collection_options
                                                                                  ssl_configured]
-        expect(account.public_settings(is_superadmin: false).size).to eq 22
+        expect(account.public_settings(is_superadmin: false).size).to eq 16
       end
       # rubocop:enable RSpec/ExampleLength
     end

1a90e087bee9045297b57129beadf0dc2edcbd9d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-12-14 16:09:29 -0500

    🎁 Add search form to splash page
    This feature introduces a search on the proprietor's home page (e.g. the
    root host of the Hyku instance).
    
    That form will then submit a search against the configured host for
    cross-tenant searching.
    
    In the case of "commonsarchive.org", that will be
    "//search.commonsarchive.org/catalog" (as demonstrated in the hard-coded value).
    
    Related to:
    
    - https://github.com/scientist-softserv/palni-palci/issues/947

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,29 @@ Bundler.require(*groups)
 
 module Hyku
   class Application < Rails::Application
+    ##
+    # @return [String] the URL to use for searching across all tenants.
+    #
+    # @see .cross_tenant_search_host
+    # @see https://github.com/scientist-softserv/palni-palci/issues/947
+    def self.cross_tenant_search_url
+      # Do not include the scheme (e.g. http or https) but instead let the browser apply the correct
+      # scheme.
+      "//#{cross_tenant_search_host}/catalog"
+    end
+
+    ##
+    # @api private
+    #
+    # @return [String] the host (e.g. "search.hykucommons.org") for cross-tenant search.
+    def self.cross_tenant_search_host
+      # I'm providing quite a few fallbacks, as this URL is used on the first page you'll see in a
+      # new Hyku instance.
+      ENV["HYKU_CROSS_TENANT_SEARCH_HOST"].presence ||
+      Account.where(search_only: true).limit(1).pluck(:cname)&.first ||
+        "search.hykucommons.org"
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

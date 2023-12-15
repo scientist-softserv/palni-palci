4d291bb46d04e6ea5adddc32e2b8fda02808e6df
LaRita Robinson <laritakr@users.noreply.github.com>
2023-12-15 10:19:55 -0500

    🧹 Get Homepage Controller specs passing (#2058)
    Refactor homepage controller to more closely align with Hyrax's
    homepage controller.
    Adjust theme views to render `modal` instead of `ajax_modal`.
    
    refs: https://github.com/scientist-softserv/hykuup_knapsack/issues/56

--- app/controllers/hyrax/homepage_controller.rb
+++ app/controllers/hyrax/homepage_controller.rb
@@ -12,15 +12,17 @@
 #
 #########################################################################################
 #########################################################################################
-# OVERRIDE: Hyrax v2.9.0 to add home_text content block to the index method - Adding themes
-# OVERRIDE: Hyrax v2.9.0 from Hyrax v2.9.0 to add facets to home page - inheriting from
-# CatalogController rather than ApplicationController
-# OVERRIDE: Hyrax v2.9.0 from Hyrax v2.9.0 to add inject_theme_views method for theming
-# OVERRIDE: Hyrax v2.9.0 to add search_action_url method from Blacklight 6.23.0 to make facet links to go to /catalog
-# OVERRIDE: Hyrax v2.9.0 to add .sort_by to return collections in alphabetical order by title on the homepage
-# OVERRIDE: Hyrax v2.9.0 add all_collections page for IR theme
-# OVERRIDE: Hyrax v2.9.0 to add facet counts for resource types for IR theme
-# OVERRIDE: Hyrax v. 2.9.0 to add @featured_collection_list to index method
+
+# OVERRIDE: Hyrax v5.0.0
+# - add home_text content block to the index method - Adding themes
+# - add inject_theme_views method for theming
+# - add all_collections page for IR theme
+# - add facet counts for resource types for IR theme
+
+# - add facets to home page - inheriting from CatalogController rather than ApplicationController
+# - add search_action_url method from Blacklight 6.23.0 to make facet links to go to /catalog
+# - add .sort_by to return collections in alphabetical order by title on the homepage
+# - add @featured_collection_list to index method
 
 module Hyrax
   # Changed to inherit from CatalogController for home page facets
@@ -61,7 +63,7 @@ module Hyrax
 
       ir_counts if home_page_theme == 'institutional_repository'
 
-      (@response, @document_list) = search_results(params)
+      (@response, @document_list) = search_service.search_results
 
       respond_to do |format|
         format.html { store_preferred_view }
@@ -102,26 +104,28 @@ module Hyrax
 
     private
 
-    # Return 6 collections
+    # Return 6 collections, sorts by title
     def collections(rows: 6)
-      builder = Hyrax::CollectionSearchBuilder.new(self)
-                                              .rows(rows)
-      response = repository.search(builder)
-      # adding .sort_by to return collections in alphabetical order by title on the homepage
-      response.documents.sort_by(&:title)
+    (response, documents) = Hyrax::CollectionsService.new(self).search_results do |builder|
+        builder.rows(rows)
+        builder.merge(sort: "title_ssi")
+      end
     rescue Blacklight::Exceptions::ECONNREFUSED, Blacklight::Exceptions::InvalidRequest
       []
     end
 
-    def recent
-      # grab any recent documents
-      (_, @recent_documents) = search_results(q: '', sort: sort_field, rows: 6)
+    # override to show 6 recent items
+    def recent(rows: 6)
+      (_, @recent_documents) = search_service.search_results do |builder|
+        builder.rows(rows)
+        builder.merge(sort: sort_field)
+      end
     rescue Blacklight::Exceptions::ECONNREFUSED, Blacklight::Exceptions::InvalidRequest
       @recent_documents = []
     end
 
     def ir_counts
-      @ir_counts = get_facet_field_response('resource_type_sim', {}, "f.resource_type_sim.facet.limit" => "-1")
+      @ir_counts = search_service.facet_field_response('resource_type_sim', "f.resource_type_sim.facet.limit" => "-1")
     end
 
     # COPIED from Hyrax::HomepageController
@@ -143,6 +147,16 @@ module Hyrax
       else
         yield
       end
+    end
+
+    # add this method to vary blacklight config and user_params
+    def search_service(*args)
+      Hyrax::SearchService.new(
+        config: ::CatalogController.new.blacklight_config, 
+        user_params: params.except(:q, :page), 
+        scope: self,
+        current_ability: current_ability,
+        search_builder_class: search_builder_class)
     end
   end
 end

38b4f8ad0687c882916df8a6880fde57b3b89649
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2024-01-04 15:05:25 -0800

    add code docs

--- app/jobs/create_large_derivatives_job.rb
+++ app/jobs/create_large_derivatives_job.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# CreateLargeDerivativesJob is intended to be used for resource-intensive derivative
+# generation (e.g. video processing). It is functionally similar to CreateDerivativesJob,
+# except that it queues jobs in the :resource_intensive queue.
+#
+# The worker responsible for processing jobs in the :resource_intensive queue should be
+# configured to have more resources dedicated to it, especially CPU. Otherwise, the
+# `ffmpeg` commands that this job class eventually triggers could be throttled.
+#
+# @see CreateDerivativesJobDecorator
+# @see Hydra::Derivatives::Processors::Ffmpeg
+# @see https://github.com/scientist-softserv/palni-palci/issues/852
 class CreateLargeDerivativesJob < CreateDerivativesJob
   queue_as :resource_intensive
 end

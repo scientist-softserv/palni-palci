ab95c9ac60a7b9adb5cfd6060ef5bf4c68ff43e3
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2024-01-03 16:00:34 -0800

    attempt to only put video and audio jobs in separate queue
    This doesn't work. This commit is only here to document that this
    solution was attempted.

--- app/jobs/create_derivatives_job_decorator.rb
+++ app/jobs/create_derivatives_job_decorator.rb
@@ -3,8 +3,12 @@
 module CreateDerivativesJobDecorator
   def self.prepended(base)
     base.class_eval do
-      queue_as :resource_intensive
+      before_perform :reassign_queue, if: ->() { arguments.first.video? || arguments.first.audio? }
     end
+  end
+
+  def reassign_queue
+    self.queue_name = 'resource_intensive'
   end
 end
 

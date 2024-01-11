47ee5ebfaa4d316db17598629c9f2f5d46525da4
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-01-11 09:43:30 -0500

    Fixing specs to reflect changes in prod code

--- spec/omni_auth/strategies/open_id_connect_decorator_spec.rb
+++ spec/omni_auth/strategies/open_id_connect_decorator_spec.rb
@@ -6,12 +6,13 @@ require 'spec_helper'
 RSpec.describe OmniAuth::Strategies::OpenIDConnectDecorator do
   let(:strategy) do
     Class.new do
-      def initialize(options: {}, session: {}, request:)
+      def initialize(options: {}, session: {}, request:, env: {})
         @options = options
         @session = session
         @request = request
+        @env = env
       end
-      attr_reader :options, :session, :request
+      attr_reader :options, :session, :request, :env
 
       delegate :params, to: :request
 
@@ -22,13 +23,17 @@ RSpec.describe OmniAuth::Strategies::OpenIDConnectDecorator do
 
   let(:requested_work_url) { "http://pals.hyku.test/concern/generic_works/f2af2a68-7c79-481b-815e-a91517e23761?locale=en" }
   let(:options) { { scope: [:openid] } }
-  let(:session) { { "cdl.requested_work_url" => requested_work_url } }
+  let(:session) { { } }
   let(:request) { double(ActionDispatch::Request, params: params) }
   let(:params) { {} }
+  let(:cookie_jar) { {} }
   let(:instance) { strategy.new(options: options, session: session, request: request) }
+
+  before { allow_any_instance_of(ActionDispatch::Request).to receive(:cookie_jar).and_return(cookie_jar) }
 
   describe '#options' do
     subject { instance.options }
+    let(:cookie_jar) { { reshare_url: requested_work_url } }
 
     it "has a :scope key that appends the #requested_work_url" do
       expect(subject.fetch(:scope)).to match_array([:openid, requested_work_url])
@@ -38,13 +43,14 @@ RSpec.describe OmniAuth::Strategies::OpenIDConnectDecorator do
   describe '#requested_work_url' do
     subject { instance.requested_work_url }
 
-    it "fetches the 'cdl.requested_work_url' session" do
-      expect(subject).to eq(requested_work_url)
+    describe "when the cookie_jar has reshare_url" do
+      let(:cookie_jar) { { reshare_url: requested_work_url } }
+      it "uses the URL in the cookie jar" do
+        expect(subject).to eq(requested_work_url)
+      end
     end
 
     describe "when the 'cdl.requested_work_url' key is missing" do
-      let(:session) { {} }
-
       context "when request does not include scope" do
         it { is_expected.to be_nil }
       end

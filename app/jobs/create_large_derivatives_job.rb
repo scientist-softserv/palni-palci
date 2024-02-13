d9f5488d587c6ed23ed1fb9dabebd7deec63072d
Benjamin Kiah Stroud <32469930+bkiahstroud@users.noreply.github.com>
2024-02-12 16:17:53 -0800

    update comments to reflect new queue name

--- app/jobs/create_large_derivatives_job.rb
+++ app/jobs/create_large_derivatives_job.rb
@@ -2,9 +2,9 @@
 
 # CreateLargeDerivativesJob is intended to be used for resource-intensive derivative
 # generation (e.g. video processing). It is functionally similar to CreateDerivativesJob,
-# except that it queues jobs in the :resource_intensive queue.
+# except that it queues jobs in the :auxiliary queue.
 #
-# The worker responsible for processing jobs in the :resource_intensive queue should be
+# The worker responsible for processing jobs in the :auxiliary queue should be
 # configured to have more resources dedicated to it, especially CPU. Otherwise, the
 # `ffmpeg` commands that this job class eventually triggers could be throttled.
 #
